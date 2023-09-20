660e23240be5f19ef1ae8065c43a3bf1d9cccd85
pythonvista <emeterv18@gmail.com>
2023-09-20 15:21:17 +0100

    fsdd

---
+++ src/components/home/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="wrap px-4 py-6 flex flex-col justify-end">
+    <div class="btns  w-full flex flex-col items-center justify-center gap-4">
+      <p class="ma-0 pa-0 text-2xl text-white font-bold">Name: Oreofe Opeyemi</p>
+      <p class="ma-0 pa-0 text-2xl text-white font-bold">Matric No: {}</p>
+      <p class="ma-0 pa-0 text-lg text-white">Solar/Inverter Recomendation App</p>    
+      <v-btn  :to="{path: '/dashboard'}" small tile>Get Started</v-btn>  
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'index'
+
+}
+</script>
+
+<style scoped>
+.wrap{
+    overflow: hidden;
+    display: flex;
+    height: 100vh;
+    background: linear-gradient(180deg, rgba(217, 217, 217, 0) 0%, #000418 93.75%), url('@/assets/img/bg.jpg');
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: cover;
+}
+
+</style>
